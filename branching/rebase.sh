c003ceb4a89fcfb66d32d9b3b0f350fddbae3107
psafonov <psafonov@secureducation.ru>
2022-04-07 22:00:14 +0500

    Revert "git-rebase 2"
    This reverts commit e061f5c3e61bf88b84056449e6be138443efde55.

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -2,8 +2,9 @@
 # display command line options
 
 count=1
-while [[ -n "$1" ]]; do
+for param in "$@"; do
     echo "Next parameter: $param"
     count=$(( $count + 1 ))
-    shift
 done
+
+echo "====="
